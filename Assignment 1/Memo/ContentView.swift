3d20175f94c61f9d498434d06319d9bb9f5d8104
patoglu <ypatoglu@icloud.com>
2021-09-13 16:01:57 +0300

    assignment 1

---
+++ Assignment 1/Memo/ContentView.swift
@@ -0,0 +1,134 @@
+//
+//  ContentView.swift
+//  Memo
+//
+//  Created by Yusuf Patoglu on 13.09.2021.
+//
+
+import SwiftUI
+
+enum EmojiSelection
+{
+    case ANIMALS, CARS, INSTRUMENTS
+}
+
+
+
+struct ContentView: View {
+    @State public var animals = ["🐟","🐬","🐳","🐋","🦈","🦭","🐊","🐅","🐆","🦓","🦍","🦧","🦣","🐘","🦛","🦏","🐪","🐫","🦒","🦘","🦬"]
+    @State var cars =
+    ["🚗","🚕","🚙","🏎️","🚓","🛻","🚚","🚛","🚔","🚡","🚃","🚋","🚞"]
+    @State var instruments =
+        ["🎻","🎸","🪗","🎺","🎷","🪘","🎹","🎨"]
+    @State var current_emojis = ["🐟","🐬","🐳","🐋","🦈","🦭","🐊","🐅","🐆","🦓","🦍","🦧","🦣","🐘","🦛","🦏","🐪","🐫","🦒","🦘","🦬"]
+    var body: some View {
+        
+        
+        VStack
+        {
+            Text("Memorize!").font(.largeTitle).fontWeight(.semibold)
+            ScrollView
+            {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 80))])
+                {
+                    ForEach(current_emojis[0..<current_emojis.capacity], id: \.self, content: {emoji in CardView(content:  emoji)}).aspectRatio(2/3, contentMode: .fit)
+                        
+                                        
+                }
+                .foregroundColor(.red)
+            }
+            HStack
+            {
+                animal_theme
+                Spacer()
+                car_theme
+                Spacer()
+                instrument_theme
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
+          
+            
+            
+        }
+        .padding(.horizontal)
+       
+        
+    }
+    var animal_theme: some View
+    {
+        Button
+        {
+            current_emojis = ["🐟","🐬","🐳","🐋","🦈","🦭","🐊","🐅","🐆","🦓","🦍","🦧","🦣","🐘","🦛","🦏","🐪","🐫","🦒","🦘","🦬"].shuffled()
+        }
+        label: {
+            Image(systemName: "hare.fill")
+        }
+    }
+    var car_theme: some View
+    {
+        Button
+        {
+            current_emojis = ["🚗","🚕","🚙","🏎️","🚓","🛻","🚚","🚛","🚔","🚡","🚃","🚋","🚞"].shuffled()
+        }
+        label: {
+            Image(systemName: "car.fill")
+        }
+    }
+    var instrument_theme: some View
+    {
+        Button
+        {
+            
+            current_emojis =       ["🎻","🎸","🪗","🎺","🎷","🪘","🎹","🎨"].shuffled()
+        }
+        label: {
+            Image(systemName: "guitars.fill")
+        }
+    }
+}
+
+
+struct CardView: View
+{
+    var content: String
+    @State var isFaceUp:Bool = true
+    var body: some View
+    {
+        ZStack
+        {
+            let shape = RoundedRectangle(cornerRadius: 20.0)
+            if(isFaceUp)
+            {
+                
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: 3.0)
+                Text(content).font(.largeTitle)
+            }
+            else
+            {
+                shape.fill()
+            }
+        }
+        .onTapGesture{
+            isFaceUp = !isFaceUp
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.light)
+        ContentView()
+            .preferredColorScheme(.dark)
+    }
+}
---
+++ Assignment 1/Memo/MemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoApp.swift
+//  Memo
+//
+//  Created by Yusuf Patoglu on 13.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct MemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
